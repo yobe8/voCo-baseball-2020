6a43fe6b7f895fa882a640f9dbb76b325f017eee
yobe8 <64393019+yobe8@users.noreply.github.com>
2020-09-21 16:20:47 +0900

    made SBO count
    얄루

--- baseBall.js
+++ baseBall.js
@@ -1,32 +1,55 @@
 const input = document.querySelector('#digits'),
-    form = document.querySelector('form');
+    form = document.querySelector('form'),
+    orderList = document.querySelector('.baseball__result__userDigits');
 
-let userDigitsArr= [],
-userDigits,
-comDigits;
+let userDigitsArr = [],
+    userDigits,
+    comDigits;
 
-function resultText() {
+function checkSBO() {
+    let strikeCount = 0,
+        ballCount = 0,
+        outCount = 0;
 
+    for (var s = 0; s < 3; s++) {
+        if(comDigits[s] === userDigits[s]) {
+            strikeCount++;
+        } else if (userDigits.includes(comDigits[s])) {
+            ballCount++;
+        } else {
+            outCount++;
+        }
+    }
+    console.log(strikeCount, ballCount, outCount);
+}
+
+function createLists() {
+    console.log('good');
+    const li = document.createElement('li')
+    li.innerText = userDigits;
+    orderList.appendChild(li);
+    checkSBO();
 }
 
 function randomDigits() {
-    const randomDigit = Math.floor(Math.random()*1000),
-    randomDigitStr = randomDigit.toString();
-    if(randomDigitStr.length === 2) {
+    const randomDigit = Math.floor(Math.random() * 1000),
+        randomDigitStr = randomDigit.toString();
+    if (randomDigitStr.length === 2) {
         randomDigits();
-    }   else if(randomDigitStr.includes('0')) {
+    } else if (randomDigitStr.includes('0')) {
         randomDigits();
     } else {
-         comDigits = randomDigitStr;
-         console.log(comDigits+' '+userDigits);
-         resultText();
+        comDigits = randomDigitStr;
+        console.log(comDigits + ' ' + userDigits);
+        createLists();
     };
 };
 
 function pushUserDigits() {
     userDigitsArr.push(userDigits);
-    input.value="";
-    userDigitsArr.length === 1 ? randomDigits() : resultText();
+    console.log(userDigitsArr);
+    input.value = "";
+    userDigitsArr.length === 1 ? randomDigits() : createLists();
 };
 
 function warningAlerts(event) {
@@ -35,7 +58,7 @@ function warningAlerts(event) {
     if (userDigits.length !== 3) {
         alert('세자리 숫자를 입력하세요(Give me 3 digits).');
     } else if (userDigits.includes('0')) {
-        alert('1 이상의 숫자를 입력하세요.(give me one or above');
+        alert('1 이상의 숫자를 입력하세요(give me one or above).');
     } else {
         pushUserDigits();
     };
